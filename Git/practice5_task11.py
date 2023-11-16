cebeeee12ffc0e9a87c43736ff275e447da63ec2
Mshukaliuk <mshukaliuk@gmail.com>
2023-11-16 15:55:17 +0000

    6th commit

--- Git/practice5_task11.py
+++ Git/practice5_task11.py
@@ -3,26 +3,48 @@ class Vehicle(object):
         self.make = make
         self.model = model
 
-    def honk(self, work:bool = False):
+    def honk(self, work: bool = False):
         if work == True:
             print("Beep!")
 
+
+"""
+Parent class for all Vehicles (Class A).
+
+    Parameters
+    ----------
+    make: str
+        Vehicle producer
+    model: str
+        Vehicle model
+
+    Returns
+    -------
+    str
+        If it works (bool == 1) - it makes "Beep!" sound.
+"""
+
+
 class LandVehicl(Vehicle):
-    def __init__(self, make, model, speed: int = 0): #якщо є додаткові параметри
+    def __init__(self, make, model, speed: int = 0):  # якщо є додаткові параметри
         super().__init__(make, model)
         self.speed = speed
 
     def drive(self, speed_increase: int):
         self.speed = self.speed + speed_increase
-        if self.speed > 0: print('Їдемо по суші')
-        elif self.speed == 0: print('Ми стоїмо')
+        if self.speed != 0:
+            print('Їдемо по суші')
+        elif self.speed <= 0:
+            print('Ми стоїмо')
 
 
-# Car: Наслідується від LandVehicle.
-# Унікальний метод: open_trunk(), який виводить "Багажник відкрито".
+"""
+Child class for LandVehicl (Class B), which is following Vehicles (Class A).
+"""
+
 
 class CarClassC(LandVehicl):
-    def __init__(self, make, model,speed):
+    def __init__(self, make, model, speed):
         self.make = make
         self.model = model
         self.speed = speed
@@ -31,21 +53,79 @@ class CarClassC(LandVehicl):
         print("Багажник відкрито")
 
 
-# Truck: Наслідується від LandVehicle.
-# Унікальний метод: load_cargo(), який виводить "Вантаж завантажено".
+"""
+Child class for CarClassC (Class C), which is following LandVehicle (Class B).
+"""
+
+
+class TruckClassC(LandVehicl):
+    def __init__(self, make, model, speed):
+        self.make = make
+        self.model = model
+        self.speed = speed
+
+    def load_cargo(self):
+        print("Вантаж завантажено")
+
+
+"""
+Child class for TruckClassC (Class C), which is following LandVehicle (Class B).
+"""
 
 
 # Підкласи від WaterVehicle:
 # Boat: Наслідується від WaterVehicle.
 # Унікальний метод: anchor(), який виводить "Човен на якорі".
-#
-# Ship: Наслідується від WaterVehicle.
-# Унікальний метод: sound_horn(), який виводить "Гудок корабля".
 
-    if __name__ == '__main__': # Code to execute
+class WaterVehicle(Vehicle):
+    def __init__(self, make, model):
+        super().__init__(make, model)
 
-            car1 = LandVehicl('Toyota', 'Camry')
-            car1.drive(10)
 
-            car2 = CarClassC('Toyota', 'Camry')
-            car2.open_car()
+class BoatClassC(WaterVehicle):
+    def __init__(self, make, model):
+        super().__init__(make, model)
+
+    def anchor(self):
+        print("Човен на якорі")
+
+
+"""
+Child class for WaterVehicle (Class B), which is following Vehicles (Class A).
+"""
+
+
+class ShipClassC(WaterVehicle):
+    def __init__(self, make, model):
+        self.make = make
+        self.model = model
+
+
+    def sound_horn(self):
+        print("Гудок корабля")
+
+
+"""
+Child class for ShipClassC (Class C), which is following WaterVehicle (Class B).
+"""
+
+#########################################################################################################################
+
+if __name__ == '__main__':  # Code to execute
+
+    car1 = LandVehicl('Toyota', 'Camry')
+    car1.honk(1)
+    car1.drive(10)
+    car1.drive(0)
+
+    car2 = CarClassC('Toyota', 'Camry', 50)
+    car2.open_car()
+
+    car3 = TruckClassC('Toyota', 'Camry', 50)
+    car3.load_cargo()
+
+    boat1 = BoatClassC('Toyota', 'Camry')
+    boat1.anchor()
+
+    ship1 = ShipClassC('Toyota', 'Camry')
+    ship1.sound_horn()
